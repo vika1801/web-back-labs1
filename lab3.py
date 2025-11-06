d1a495e33cf8c61f3d2a37cb2d3e839b5c6e2794
vika1801 <vksspv@gmail.com>
2025-11-07 02:56:07 +0700

    Валидация

--- lab3.py
+++ lab3.py
@@ -19,17 +19,22 @@ def cookie():
 
 @lab3.route('/lab3/del_cookie')
 def del_cookie():
-      resp = make_response(redirect('/lab3/'))
-      resp.delete_cookie('name')
-      resp.delete_cookie('age')
-      resp.delete_cookie('name_color')
-      return resp
+    resp = make_response(redirect('/lab3/'))
+    resp.delete_cookie('name')
+    resp.delete_cookie('age')
+    resp.delete_cookie('name_color')
+    return resp
 
 
 
 @lab3.route('/lab3/form1')
 def form1():
-      user = request.args.get('user')
-      age = request.args.get('age')
-      sex = request.args.get('sex')
-      return render_template('lab3/form1.html', user=user, age=age, sex=sex)
+    errors={}
+    user = request.args.get('user')
+    if user == '':
+        errors['user']  = 'Заполните поле!!!'
+    age = request.args.get('age')
+    if age == '':
+        errors['age']  = 'Заполните поле!!!'
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
