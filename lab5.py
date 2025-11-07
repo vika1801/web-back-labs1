88ae0489c9f3d82865bd9e7ea542dfa73f52dc6b
vika1801 <vksspv@gmail.com>
2025-11-07 08:43:55 +0700

    Правки для SQLite

--- lab5.py
+++ lab5.py
@@ -129,8 +129,16 @@ def create():
     title = request.form.get('title')
     article_text = request.form.get('article_text')
     conn, cur = db_connect()
-    cur.execute('SELECT login FROM users WHERE login=%s;', (login, ))
+    
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute('SELECT login FROM users WHERE login=%s;', (login, ))
+    else:
+        cur.execute('SELECT login FROM users WHERE login=?;', (login, ))
     user_id = cur.fetchone()['id']
-    cur.execute("INSERT INTO articles (user_id, title, article_text) VALUES (%s, %s, %s);", (user_id, title, article_text))
+    
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("INSERT INTO articles (user_id, title, article_text) VALUES (%s, %s, %s);", (user_id, title, article_text))
+    else:
+        cur.execute("INSERT INTO articles (user_id, title, article_text) VALUES (?, ?, ?);", (user_id, title, article_text))
     db_close(conn, cur)
     return redirect ('/lab5')
