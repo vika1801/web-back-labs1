9fee1d3d86fd666ab225dead2a8ca3d2309aa7b8
vika1801 <vksspv@gmail.com>
2025-11-07 08:43:34 +0700

    Правки для SQLite

--- lab5.py
+++ lab5.py
@@ -48,7 +48,10 @@ def login():
     
     conn, cur = db_connect()
     
-    cur.execute("SELECT * FROM users WHERE login=%s", (login,))
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT * FROM users WHERE login=%s", (login,))
+    else:
+        cur.execute("SELECT * FROM users WHERE login=?", (login,))
     user = cur.fetchone()
     if not user:
         db_close(conn, cur)
@@ -74,13 +77,20 @@ def register():
 
     conn, cur = db_connect()
 
-    cur.execute("SELECT login FROM users WHERE login=%s;",(login,))
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT login FROM users WHERE login=%s;",(login,))
+    else:
+        cur.execute("SELECT login FROM users WHERE login=?;",(login,))
+
     if cur.fetchone():
         db_close(conn, cur)
         return render_template('lab5/register.html', error= 'Такой пользователь уже существует')
 
     password_hash = generate_password_hash(password)
-    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login, password_hash) )
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login, password_hash))
+    else:
+        cur.execute("INSERT INTO users (login, password) VALUES (?, ?);", (login, password_hash))
 
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
@@ -92,9 +102,17 @@ def list():
     if not login: 
         return redirect('lab5/login')
     conn, cur = db_connect()
-    cur.execute("SELECT id FROM users WHERE login=%s;", (login,))
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT id FROM users WHERE login=%s;", (login,))
+    else:
+        cur.execute("SELECT id FROM users WHERE login=?;", (login,))
+    
     user_id = cur.fetchone()["id"]
-    cur.execute("SELECT * FROM articles WHERE user_id=%s;", (user_id,))
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT * FROM articles WHERE user_id=%s;", (user_id,))
+    else:
+        cur.execute("SELECT * FROM articles WHERE user_id=?;", (user_id,))
+
     articles = cur.fetchall()
     db_close(conn, cur)
     return render_template ('/lab5/articles.html', articles=articles)
