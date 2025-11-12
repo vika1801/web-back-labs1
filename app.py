e21ff633f65d86315170b8c01591de21c1db1144
Вика Сопова <vksspv@gmail.com>
2025-11-12 16:06:26 +0700

    Добавление меню

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3) 
 app.register_blueprint(lab4) 
 app.register_blueprint(lab5) 
+app.register_blueprint(lab6)
 
 @app.route("/")
 @app.route('/index')
