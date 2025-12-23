a439c6233172a43a2e0c1718f64cb95e93168f1a
Вика Сопова <vksspv@gmail.com>
2025-12-23 20:36:50 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, url_for, request, redirect, make_response, render_template, abort
 import datetime
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
 
 from lab1 import lab1
 from lab2 import lab2
@@ -15,6 +18,22 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'cекретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'vika_sopova_orm'
+    db_user = 'vika_sopova_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else: 
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "vika_sopova_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1) 
 app.register_blueprint(lab2) 
