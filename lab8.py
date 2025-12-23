8d6f7b38e5090b2f9790cd0c478b1902d6a758d5
Вика Сопова <vksspv@gmail.com>
2025-12-23 22:30:41 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, session, current_app
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from db import db
 from db.models import users, articles
+from flask_login import login_user, login_required, current_user
 
 lab8 = Blueprint('lab8', __name__)  
 
@@ -13,7 +14,30 @@ def main():
 def login():
     if request.method == 'GET':
         return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
 
+    if not login_form:
+        return render_template('lab8/login.html', 
+                               error='Введите логин',
+                               login_form=login_form)
+    
+    if not password_form:
+        return render_template('lab8/login.html', 
+                               error='Введите пароль',
+                               login_form=login_form)
+
+    user = users.query.filter_by(login = login_form).first()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember = False)
+            return redirect('/lab8/')
+        
+    return render_template('/lab8/login.html',
+                            error = 'Ошибка входа: логин и/или пароль неверны')
+    
 @lab8.route('/lab8/register/', methods = ['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -41,3 +65,8 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     return redirect('/lab8/')
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "Список статей"
